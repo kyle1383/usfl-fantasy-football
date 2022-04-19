5488ce6f5d3e6155424c14c44d27e822239034fb
Kyle Morand <kmorand@me.com>
2022-04-18 20:22:52 -0400

    form styles

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,9 +1,10 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import Form from "react-validation/build/form";
 import Input from "react-validation/build/input";
 import CheckButton from "react-validation/build/button";
 import AuthService from "../../services/auth.service";
+
 const required = (value) => {
   if (!value) {
     return (
@@ -29,6 +30,12 @@ const Login = () => {
     const password = e.target.value;
     setPassword(password);
   };
+  useEffect(() => {
+    if (AuthService.isLoggedIn()) {
+      navigate(-1) || navigate("/dashboard");
+    }
+  }, []);
+
   const handleLogin = (e) => {
     e.preventDefault();
     setMessage("");
@@ -55,59 +62,63 @@ const Login = () => {
       setLoading(false);
     }
   };
-  return (
-    <div className="login">
-      <div className="welcome">Welcome to USFL Fantasy</div>
-      <div className="form-box">
-        <Form onSubmit={handleLogin} ref={form}>
-          <div className="form-group">
-            <Input
-              type="text"
-              placeholder="username"
-              className="form-control name"
-              name="username"
-              value={username}
-              onChange={onChangeUsername}
-              validations={[required]}
-            />
-
-            <Input
-              type="password"
-              placeholder="password"
-              className="form-control password"
-              name="password"
-              value={password}
-              onChange={onChangePassword}
-              validations={[required]}
-            />
-          </div>
-          <div className="form-group">
-            <button className="btn btn-submit " disabled={loading}>
-              {loading && (
-                <span className="spinner-border spinner-border-sm"></span>
-              )}
-              <span>Login</span>
-            </button>
-          </div>
-          <div className="don't have an account">
-            Dont have an account?
-            <Link to={"/register"} className="nav-link">
-              {" "}
-              Register
-            </Link>
-          </div>
-
-          {message && (
+  if (!AuthService.isLoggedIn()) {
+    return (
+      <div className="login">
+        <div className="welcome">Welcome to USFL Fantasy</div>
+        <div className="form-box">
+          <Form onSubmit={handleLogin} ref={form}>
             <div className="form-group">
-              <div className="alert alert-danger" role="alert">
-                {message}
-              </div>
+              <Input
+                type="text"
+                placeholder="username"
+                className="form-control name"
+                name="username"
+                value={username}
+                onChange={onChangeUsername}
+                validations={[required]}
+              />
+
+              <Input
+                type="password"
+                placeholder="password"
+                className="form-control password"
+                name="password"
+                value={password}
+                onChange={onChangePassword}
+                validations={[required]}
+              />
             </div>
-          )}
-          <CheckButton style={{ display: "none" }} ref={checkBtn} />
-        </Form>
+            <div className="form-group">
+              <button className="btn btn-submit " disabled={loading}>
+                {loading && (
+                  <span className="spinner-border spinner-border-sm"></span>
+                )}
+                <span>Login</span>
+              </button>
+            </div>
+            <div className="don't have an account">
+              Dont have an account?
+              <Link to={"/register"} className="nav-link">
+                {" "}
+                Register
+              </Link>
+            </div>
+
+            {message && (
+              <div className="form-group">
+                <div className="alert alert-danger" role="alert">
+                  {message}
+                </div>
+              </div>
+            )}
+            <CheckButton style={{ display: "none" }} ref={checkBtn} />
+          </Form>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return null;
+  }
 };
 export default Login;
